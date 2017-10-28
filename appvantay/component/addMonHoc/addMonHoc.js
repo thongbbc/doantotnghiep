0852aa3729d8c9ef430bfa8c275f074f63f7b125
nguyenanhthong <nguyenanhthong1995@gmail.com>
2017-10-28 23:28:13 +0700

    Update

--- appvantay/component/addMonHoc/addMonHoc.js
+++ appvantay/component/addMonHoc/addMonHoc.js
@@ -8,7 +8,7 @@ import {
   Alert,
   Picker,
   Animated,
-  Keyboard,
+  Keyboard,DatePickerAndroid,
   DatePickerIOS,
   TextInput,TimePickerAndroid,
   Dimensions,
@@ -26,6 +26,9 @@ export default class AddMonHoc extends Component {
     super(props)
     this.springValue1 = new Animated.Value(1)
     this.springValue2 = new Animated.Value(1)
+    this.springValue3 = new Animated.Value(1)
+    this.springValue4 = new Animated.Value(1)
+
     dulieu = []
     thu = [
       'Mon',
@@ -36,6 +39,7 @@ export default class AddMonHoc extends Component {
       'Sat',
       'Sun'
     ]
+    const dateNew = new Date()
     this.state = {
       them: '',
       animating: false,
@@ -43,10 +47,15 @@ export default class AddMonHoc extends Component {
       dataSource: [],
       tenMonHoc: '',
       selectedThu: 'Mon',
-      timeStart: new Date(),
-      timeEnd: new Date(),
-      startString: '0:00',
-      endString: '0:00',
+      timeStart: dateNew,
+      timeEnd: dateNew,
+      startString: dateNew.getHours().toString()+':'+dateNew.getMinutes().toString(),
+      endString: dateNew.getHours().toString()+':'+dateNew.getMinutes().toString(),
+      dateStart: dateNew,
+      dateEnd: dateNew,
+      dateStartString:dateNew.getDate().toString()+'/'+(dateNew.getMonth()+1).toString()+'/'+dateNew.getFullYear().toString(),
+      dateEndString:dateNew.getDate().toString()+'/'+(dateNew.getMonth()+1).toString()+'/'+dateNew.getFullYear().toString(),
+      hideDatePicker:true,
       choose: true,
       hide: true
     }
@@ -65,10 +74,24 @@ export default class AddMonHoc extends Component {
       friction: 2
     }).start()
   }
+  spring3() {
+    this.springValue3.setValue(0.3)
+    Animated.spring(this.springValue3, {
+      toValue: 1,
+      friction: 2
+    }).start()
+  }
+  spring4() {
+    this.springValue4.setValue(0.3)
+    Animated.spring(this.springValue4, {
+      toValue: 1,
+      friction: 2
+    }).start()
+  }
   componentDidMount() {}
   //getAll ID student
   fetchData = async() => {
-    fetch(`https://doantotnghiep.herokuapp.com/saveMonHoc/?tenmonhoc=${this.state.tenMonHoc}&timestart=${this.state.startString}&timeend=${this.state.endString}&thu=${this.state.selectedThu}`, {method: 'GET'}).then((response) => response.json()).then((responseJson) => {
+    fetch(`https://doantotnghiep.herokuapp.com/saveMonHoc/?tenmonhoc=${this.state.tenMonHoc}&timestart=${this.state.startString}&timeend=${this.state.endString}&thu=${this.state.selectedThu}&datestart=${this.state.dateStartString}&dateend=${this.state.dateEndString}`, {method: 'GET'}).then((response) => response.json()).then((responseJson) => {
       if (responseJson.status == 'OK') {
         Alert.alert("Add Môn Học Thành Công");
       } else {
@@ -98,11 +121,30 @@ export default class AddMonHoc extends Component {
       });
     }
   }
+  onDatePickerChange(date) {
+    const {choose: luachon} = this.state
+    if (luachon == true) {
+      this.setState({
+        dateStart: date,
+        dateStartString: date.getDate().toString() + '/' + (date.getMonth()+1).toString()+"/"+(date.getFullYear()).toString()
+      });
+    } else {
+      this.setState({
+        dateEnd: date,
+        dateEndString: date.getDate().toString() + '/' + (date.getMonth()+1).toString()+"/"+(date.getFullYear()).toString()
+      });
+    }
+  }
   onClickAddMonHoc() {
-    this.fetchData()
+    const {dateStartString,dateEndString,tenMonHoc,startString,endString} = this.state
+    if (dateStartString!='' && dateEndString!='' && tenMonHoc!='' && startString!='' && endString!='') {
+      this.fetchData()
+    } else {
+      alert("PLEASE PRESS FULL INFORMATION")
+    }
   }
   _onPressHideDatePicker() {
-    this.setState({hide: true})
+    this.setState({hide: true,hideDatePicker:true})
   }
   //Hiển thị DatePickerIOS
   checkHide() {
@@ -175,6 +217,116 @@ export default class AddMonHoc extends Component {
       }
     }
   }
+  checkDate() {
+    const {hideDatePicker} = this.state
+    if (hideDatePicker == false) {
+      if (Platform.OS == 'ios') {
+        return (
+          <View style={{
+            position: 'absolute',
+            height: Dimensions.get('window').height,
+            justifyContent: 'flex-end'
+          }}>
+            <View style={{
+              flex: 1
+            }}>
+              <TouchableHighlight onPress={this._onPressHideDatePicker.bind(this)} style={{
+                flex: 1
+              }}>
+                <View style={{
+                  flex: 1
+                }}></View>
+              </TouchableHighlight>
+            </View>
+            <View style={{
+              backgroundColor: 'white',
+              height: null
+            }}>
+              <View style={{
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                width: Dimensions.get('window').width,
+                height: 30,
+                backgroundColor: '#D2D5DA'
+              }}>
+                <TouchableHighlight onPress={this._onPressHideDatePicker.bind(this)} style={{
+                  marginLeft: 5,
+                  justifyContent: 'center',
+                  height: 30
+                }}>
+                  <Text style={{
+                    fontSize: 15,
+                    color: 'blue'
+                  }}>Cancel</Text>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={this._onPressHideDatePicker.bind(this)} style={{
+                  marginRight: 5,
+                  justifyContent: 'center',
+                  height: 30
+                }}>
+                  <Text style={{
+                    fontSize: 15,
+                    color: 'blue'
+                  }}>OK</Text>
+                </TouchableHighlight>
+              </View>
+              <View>
+                <DatePickerIOS date={this.state.choose == true
+                  ? this.state.dateStart
+                  : this.state.dateEnd} mode="date" timeZoneOffsetInMinutes={7 * 60} onDateChange={(date) => this.onDatePickerChange(date)}/>
+              </View>
+            </View>
+          </View>
+        )
+      } else {
+        if (this.state.choose == true) {
+          this.openDatePickerAndroid2(true)
+        } else {
+          this.openDatePickerAndroid2(false)
+        }
+      }
+    }
+  }
+  async openDatePickerAndroid2(check) {
+    k = new Date()
+    if (check == true){
+      try {
+        const {action, year, month, day} = await DatePickerAndroid.open({
+          date: new Date(2020, 4, 25)
+
+            });
+            if (action === DatePickerAndroid.dateSetAction) {
+              k.setDate(date.getDate())
+              k.setMonth(date.getMonth())
+              k.setFullYear(date.getFullYear())
+              this.setState({hideDatePicker:true,dateStartString:k.getDate()+'/'+(k.getMonth()+1)+'/'+
+                k.getFullYear(),dateStart:k})
+            } else if (action === DatePickerAndroid.dismissedAction) {
+
+            }
+          } catch ({code, message}) {
+            console.warn('Cannot open time picker', message);
+          }
+    } else {
+      try {
+        const {action, year, month, day} = await DatePickerAndroid.open({
+          date: new Date(2020, 4, 25)
+
+            });
+            if (action === DatePickerAndroid.dateSetAction) {
+              k.setDate(date.getDate())
+              k.setMonth(date.getMonth())
+              k.setFullYear(date.getFullYear())
+              this.setState({hideDatePicker:true,dateEndString:k.getDate()+'/'+(k.getMonth()+1)+'/'+
+                k.getFullYear(),dateEnd:k})
+            } else if (action === DatePickerAndroid.dismissedAction) {
+
+            }
+          } catch ({code, message}) {
+            console.warn('Cannot open time picker', message);
+          }
+    }
+  }
   async openDatePickerAndroid(check) {
     k = new Date()
     if (check == true){
@@ -223,6 +375,16 @@ export default class AddMonHoc extends Component {
     this.setState({hide: false, choose: false})
     Keyboard.dismiss()
     this.spring2()
+  }
+  _onPressDateStart() {
+    this.setState({hideDatePicker: false, choose: true})
+    Keyboard.dismiss()
+    this.spring3()
+  }
+  onPressDateEnd() {
+    this.setState({hideDatePicker: false, choose: false})
+    Keyboard.dismiss()
+    this.spring4()
   }
   render() {
     return (
@@ -290,10 +452,10 @@ export default class AddMonHoc extends Component {
               textAlign: 'left',
               fontSize: 12,paddingTop:10,paddingBottom:10,
               fontWeight: 'bold'
-            }}>Time Start</Text>
+            }}>Time Start & Date Start</Text>
             <View style={{
-              width: Dimensions.get('window').width,
-              alignItems: 'center'
+              width: Dimensions.get('window').width,flexDirection:'row',
+              alignItems: 'center',justifyContent:'center'
             }}>
               {Platform.OS == 'ios'
                 ? <Animated.Text onPress={this.onPressTimeStart.bind(this)} style={{
@@ -304,9 +466,9 @@ export default class AddMonHoc extends Component {
                     ],
                     fontWeight: 'bold',
                     padding: 5,
-                    fontSize: 13,
+                    fontSize: 13,right:10,
                     borderRadius:20,
-                    width: Dimensions.get('window').width - 60,
+                    width: (Dimensions.get('window').width - 60-5)/2,
                     height: (Dimensions.get('window').height / 2 - 90) / 6,
                     backgroundColor: "rgba(255,255,255,0.5)",
                     color: "black"
@@ -318,15 +480,49 @@ export default class AddMonHoc extends Component {
                     }
                   ],
                   fontWeight: 'bold',
-                  width: Dimensions.get('window').width - 60,
+                  width: (Dimensions.get('window').width - 60-5)/2,
                   padding: 5,
-                  borderRadius:20,
+                  borderRadius:20,right:10,
                   fontSize: 13,
                   backgroundColor: 'transparent',
                   height: (Dimensions.get('window').height / 2 - 90) / 6,
                   backgroundColor: "rgba(255,255,255,0.5)",
                   color: "black"
                 }} underlineColorAndroid={"transparent"}>{this.state.timeStart.getHours().toString()}:{(this.state.timeStart.getMinutes()).toString()}</Animated.Text>}
+
+                {Platform.OS == 'ios'
+                  ? <Animated.Text onPress={this._onPressDateStart.bind(this)} style={{
+                      transform: [
+                        {
+                          scale: this.springValue3
+                        }
+                      ],
+                      fontWeight: 'bold',
+                      padding: 5,
+                      fontSize: 13,
+                      borderRadius:20,
+                      width: (Dimensions.get('window').width - 60)/2,
+                      height: (Dimensions.get('window').height / 2 - 90) / 6,
+                      backgroundColor: "rgba(255,255,255,0.5)",
+                      color: "black"
+                    }} underlineColorAndroid={"transparent"}>{this.state.dateStart.getDate().toString()}/{(this.state.dateStart.getMonth()+1).toString()}/{(this.state.dateStart.getFullYear()).toString()}</Animated.Text>
+                  : <Animated.Text onPress={this._onPressDateStart.bind(this)} style={{
+                    transform: [
+                      {
+                        scale: this.springValue3
+                      }
+                    ],
+                    fontWeight: 'bold',
+                    width: (Dimensions.get('window').width - 60)/2,
+                    padding: 5,
+                    borderRadius:20,
+                    fontSize: 13,
+                    backgroundColor: 'transparent',
+                    height: (Dimensions.get('window').height / 2 - 90) / 6,
+                    backgroundColor: "rgba(255,255,255,0.5)",
+                    color: "black"
+                  }} underlineColorAndroid={"transparent"}>{this.state.dateStart.getDate().toString()}/{(this.state.dateStart.getMonth()+1).toString()}/{(this.state.dateStart.getFullYear()).toString()}</Animated.Text>
+              }
             </View>
             <Text style={{
               left: 30,
@@ -335,11 +531,13 @@ export default class AddMonHoc extends Component {
               backgroundColor:'transparent',
               fontSize: 12,paddingTop:10,paddingBottom:10,
               fontWeight: 'bold'
-            }}>Time End</Text>
+            }}>Time End & Date End</Text>
             <View style={{
               width: Dimensions.get('window').width,
-              alignItems: 'center'
+              alignItems: 'center',
             }}>
+            <View style={{width:Dimensions.get('window').width,flexDirection:'row',justifyContent:'center',
+            alignItems:'center'}}>
               {Platform.OS == 'ios'
                 ? <Animated.Text onPress={this.onPressTimeEnd.bind(this)} style={{
                     transform: [
@@ -349,9 +547,9 @@ export default class AddMonHoc extends Component {
                     ],
                     fontWeight: 'bold',
                     padding: 5,
-                    borderRadius:20,
+                    borderRadius:20,right:10,
                     fontSize: 13,
-                    width: Dimensions.get('window').width - 60,
+                    width: (Dimensions.get('window').width - 60 -5)/2,
                     height: (Dimensions.get('window').height / 2 - 90) / 6,
                     backgroundColor: "rgba(255,255,255,0.5)",
                     color: "black"
@@ -363,15 +561,51 @@ export default class AddMonHoc extends Component {
                     }
                   ],
                   fontWeight: 'bold',
-                  width: Dimensions.get('window').width - 60,
+                  width: (Dimensions.get('window').width - 60 -5)/2,
                   padding: 5,
                   fontSize: 13,
-                  borderRadius:20,
+                  borderRadius:20,right:10,
                   backgroundColor: 'transparent',
                   height: (Dimensions.get('window').height / 2 - 90) / 6,
                   backgroundColor: "rgba(255,255,255,0.5)",
                   color: "black"
                 }} underlineColorAndroid={"transparent"}>{this.state.timeEnd.getHours().toString()}:{(this.state.timeEnd.getMinutes()).toString()}</Animated.Text>}
+
+
+                {Platform.OS == 'ios'
+                  ? <Animated.Text onPress={this.onPressDateEnd.bind(this)} style={{
+                      transform: [
+                        {
+                          scale: this.springValue4
+                        }
+                      ],
+                      fontWeight: 'bold',
+                      padding: 5,
+                      borderRadius:20,
+                      fontSize: 13,
+                      width: (Dimensions.get('window').width - 60 -5)/2,
+                      height: (Dimensions.get('window').height / 2 - 90) / 6,
+                      backgroundColor: "rgba(255,255,255,0.5)",
+                      color: "black"
+                    }} underlineColorAndroid={"transparent"}>{this.state.dateEnd.getDate().toString()}/{(this.state.dateEnd.getMonth()+1).toString()}/{this.state.dateEnd.getFullYear().toString()}</Animated.Text>
+                  : <Animated.Text onPress={this.onPressDateEnd.bind(this)} style={{
+                    transform: [
+                      {
+                        scale: this.springValue4
+                      }
+                    ],
+                    fontWeight: 'bold',
+                    width: (Dimensions.get('window').width - 60 -5)/2,
+                    padding: 5,
+                    fontSize: 13,
+                    borderRadius:20,
+                    backgroundColor: 'transparent',
+                    height: (Dimensions.get('window').height / 2 - 90) / 6,
+                    backgroundColor: "rgba(255,255,255,0.5)",
+                    color: "black"
+                  }} underlineColorAndroid={"transparent"}>{this.state.dateEnd.getDate().toString()}/{(this.state.dateEnd.getMonth()+1).toString()}/{this.state.dateEnd.getFullYear().toString()}</Animated.Text>
+              }
+                </View>
                 <View style={{width: Dimensions.get('window').width - 60}}>
                   <Picker selectedValue={this.state.selectedThu} onValueChange={(thu) => this.setState({selectedThu: thu})}>
                     {this._renderItem()}
@@ -383,6 +617,7 @@ export default class AddMonHoc extends Component {
         </View>
         </LinearGradient>
         {this.checkHide()}
+        {this.checkDate()}
       </View>
     );
   }
